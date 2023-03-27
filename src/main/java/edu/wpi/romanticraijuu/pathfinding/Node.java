c52ce8ff46250f3a88eb18c21f417fc30422529c
Alton Miles <altonmiles@autoreg-5289524.dyn.wpi.edu>
2023-03-26 22:00:17 -0400

    Created java classes for the node table in the database

--- src/main/java/edu/wpi/romanticraijuu/pathfinding/Node.java
+++ src/main/java/edu/wpi/romanticraijuu/pathfinding/Node.java
@@ -4,25 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 @Getter @Setter
 public class Node {
-    private enum floor {
-        L1, L2
-    }
-    private  enum nodeType {
-        CONF, DEPT, HALL, LABS, REST, RETL, SERV, ELEV, EXIT, STAI
-    }
+
     private int xCoord, yCoord;
-    private String nodeID, longName, shortName, building;
-    private floor buildingFloor;
-    private nodeType nodeType;
-    public Node(String nodeID, int xCoord, int yCoord, String longName, String shortName, String building, nodeType aType, floor aFloor) {
+    private String nodeID, longName, shortName, building, buildingFloor, nodeType;
+    public Node(String nodeID, int xCoord, int yCoord, String buildingFloor, String building, String nodeType, String longName, String shortName) {
         this.nodeID = nodeID;
         this.xCoord = xCoord;
         this.yCoord = yCoord;
+        this.building = buildingFloor;
+        this.buildingFloor = building;
+        this.nodeType = nodeType;
         this.longName = longName;
         this.shortName = shortName;
-        this.building = building;
-        this.buildingFloor = aFloor;
-        this.nodeType = aType;
     }
 
 
