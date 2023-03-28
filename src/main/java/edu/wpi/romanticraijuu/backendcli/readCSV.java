317223bc16d04b012fa668de972fd15a455be5b4
SimonAJScott <simon.aj.scott@gmail.com>
2023-03-28 02:49:57 -0400

    filled in methods in readCSV

--- src/main/java/edu/wpi/romanticraijuu/backendcli/readCSV.java
+++ src/main/java/edu/wpi/romanticraijuu/backendcli/readCSV.java
@@ -1,38 +1,55 @@
 package edu.wpi.romanticraijuu.backendcli;
 import java.sql.Connection;
 import java.io.*;
-import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 import java.sql.Statement;
-import edu.wpi.romanticraijuu.pathfinding.*;
 public class readCSV {
     Statement statement;
     String filePathSlashName;
-    public static void readCSVFile(String fileName, Connection connection) throws Exception {
+    public void readCSVFile(String filePathSlashName, Connection connection) throws Exception {
         Statement aStatement = connection.createStatement();
+        this.filePathSlashName = filePathSlashName;
     }
 
-    public void importIntoNode() throws FileNotFoundException {
+    public void importIntoNode() throws FileNotFoundException, SQLException { //does .next() move the scanner to the next character?
         Scanner sc = new Scanner(new File(filePathSlashName));
         sc.useDelimiter(",");
         while (sc.hasNextLine()) {
-            String currentLine = sc.nextLine();
-            try(Scanner rowScanner = new Scanner(currentLine)){
-                rowScanner.useDelimiter(",");
-                while(rowScanner.hasNext()){
-
-                }
-            }
+            String nodeID = sc.next();
+            int xCoord = sc.nextInt();
+            int yCoord = sc.nextInt();
+            String longName = sc.next();
+            String shortName = sc.next();
+            String building = sc.next();
+            String buildingFloor = sc.next();
+            String nodeType = sc.next();
+            statement.executeUpdate("insert into node"+ "(nodeID, xcoord, ycoord, buildingFloor, building, nodeType, longName, shortName) VALUES ('" + nodeID + "', " + xCoord + ", " + yCoord + ", '" + buildingFloor + "', '" + building + "', " + nodeType + "', " + longName + "', '" + shortName + "');");
         }
         sc.close();
     }
 
-    public void importIntoNodeType(){
-
+    public void importIntoNodeType() throws FileNotFoundException, SQLException { //does .next() move the scanner to the next character?
+        Scanner sc = new Scanner(new File(filePathSlashName));
+        sc.useDelimiter(",");
+        while(sc.hasNextLine()){
+            String nodeType = sc.next();
+            statement.executeUpdate("INSERT INTO nodeType(nodeType) VALUES('"+nodeType+"');");
+        }
+        sc.close();
     }
 
-    public void importIntoEdge(){
 
+    public void importIntoEdge() throws FileNotFoundException, SQLException { //does .next() move the scanner to the next character?
+        Scanner sc = new Scanner(new File(filePathSlashName));
+        sc.useDelimiter(",");
+        while(sc.hasNextLine()){
+            String edgeID = sc.next();
+            String startNode = sc.next();
+            String endNode = sc.next();
+            statement.executeUpdate("INSERT INTO edge(edgeID, startNode, endNode) VALUES ('"+edgeID+"','"+startNode+"','"+endNode+"');");
+        }
+        sc.close();
     }
 
 }
