3717778c81eaaeb54dec6a860040c715c0cfdc73
Alton Miles <altonmiles@vpnclient-10-217-65-11.int.wpi.edu>
2023-03-28 19:47:20 -0400

    Hooked main up with CLI function

--- src/main/java/edu/wpi/romanticraijuu/Main.java
+++ src/main/java/edu/wpi/romanticraijuu/Main.java
@@ -1,9 +1,12 @@
 package edu.wpi.romanticraijuu;
 
+import edu.wpi.romanticraijuu.backendcli.CLI;
+
 public class Main {
 
   public static void main(String[] args) {
-    App.launch(App.class, args);
+    // App.launch(App.class, args);
+    CLI.cli();
   }
 
   // shortcut: psvm
