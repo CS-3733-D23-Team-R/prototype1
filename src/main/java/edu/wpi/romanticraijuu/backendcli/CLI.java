89d7ba785cfae92518d476eef0cb0e8b4c1a5a62
Alton Miles <altonmiles@Altons-Air.lan>
2023-03-28 13:05:17 -0400

    More CLI work to implement other functions

--- src/main/java/edu/wpi/romanticraijuu/backendcli/CLI.java
+++ src/main/java/edu/wpi/romanticraijuu/backendcli/CLI.java
@@ -10,7 +10,7 @@ public class CLI {
     private static Scanner scanner;
     private static NodeDAO nodeDAO;
     private static EdgeDAO edgeDAO;
-    public static void cli(String[] args){
+    public static void cli(){
         scanner = new Scanner(System.in);
         System.out.println("CLI Started");
         System.out.println("Connecting to the database");
@@ -78,7 +78,8 @@ public class CLI {
                 8- Import a csv file into the node table
                 9- Display help on how to use this program
                 10- Exit the program""", new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"});
-
+        String userNodeID;
+        String userEdgeID;
         switch (input){
             case "1":
                 System.out.println("\nList of nodes:\n");
@@ -91,9 +92,9 @@ public class CLI {
                     System.out.println("EdgeID: " + edge.getEdgeID());
                 break;
             case "3":
-                String userNode = getGeneralInput("Enter the NodeID of the desired node");
+                userNodeID = getGeneralInput("Enter the NodeID of the desired node");
                 try{
-                    Node node = nodeDAO.getNodeByID(userNode);
+                    Node node = nodeDAO.getNodeByID(userNodeID);
                     System.out.println("The node's ID is: " + node.getNodeID());
                     System.out.println("The node's coordinates are: (" + node.getXCoord() + ", " + node.getYCoord() + ")");
                     System.out.println("The node's floor is: " + node.getBuildingFloor());
@@ -106,9 +107,9 @@ public class CLI {
                 }
                 break;
             case "4":
-                String userEdge = getGeneralInput("Enter the EdgeID of the desired edge");
+                userEdgeID = getGeneralInput("Enter the EdgeID of the desired edge");
                 try{
-                    Edge edge = edgeDAO.getEdgeByID(userEdge);
+                    Edge edge = edgeDAO.getEdgeByID(userEdgeID);
                     System.out.println("The edge's ID is: " + edge.getEdgeID());
                     System.out.println("The edge's starting node's ID is: " + edge.getStartNode());
                     System.out.println("The edge's ending node's ID is: " + edge.getEndNode());
@@ -117,14 +118,48 @@ public class CLI {
                 }
                 break;
             case "5":
-                String userNodeCoords = getGeneralInput("Enter the NodeID of node you wish to modify");
+                userNodeID = getGeneralInput("Enter the NodeID of node you wish to modify");
+                int xCoord = getIntegerInput("Enter the new X-Coordinate: ");
+                int yCoord = getIntegerInput("Enter the new Y-Coordinate: ");
+                try {
+                    nodeDAO.modifyNodeXCoordByID(userNodeID, xCoord);
+                    nodeDAO.modifyNodeYCoordByID(userNodeID, yCoord);
+                } catch (SQLException e) {
+                    System.out.println("There was an error with accessing the database");
+                } catch (TupleNotFoundException e) {
+                    System.out.println("Node not found. Please double check your information");
+                }
                 break;
             case "6":
+                userNodeID = getGeneralInput("Enter the NodeID of node you wish to modify");
+                String longName = getGeneralInput("Enter the new name");
+                try{
+                    nodeDAO.modifyNodeLongNameByID(userNodeID, longName);
+                } catch (SQLException e) {
+                    System.out.println("There was an error with accessing the database");
+                } catch (TupleNotFoundException e) {
+                    System.out.println("Node not found. Please double check your information");
+                }
 
                 break;
             case "7":
+                String outputCSVFilePath = getGeneralInput("Enter the desired output csv file");
+                //TODO: Sync with readCSV
             case "8":
-            case "9":
+                String csvFilePath = getGeneralInput("Enter the path to your CSV file containing the new node information");
+                String confirmation = getSpecificInput("This will delete add data stored within the node table. \nEnter \"yes\" to confirm or \"no\" to cancel the change."
+                , new String[]{"yes", "no"});
+
+                if (confirmation.equals("no")) //removes user from the menu if they don't want to remove data
+                    return;
+
+                //TODO: Sync with writeCSV
+
+
+            case "9": //TODO: FINISH WHEN THE CLI IS FINALIZED
+                System.out.println("""
+                        Help Menu:
+                        """);
             case "10":
                 return;
         }
@@ -151,5 +186,12 @@ public class CLI {
             System.out.println(prompt);
 
         return scanner.next();
+    }
+
+    private static int getIntegerInput(String prompt){
+        if (!prompt.equals(""))
+            System.out.println(prompt);
+
+        return scanner.nextInt();
     }
 }
