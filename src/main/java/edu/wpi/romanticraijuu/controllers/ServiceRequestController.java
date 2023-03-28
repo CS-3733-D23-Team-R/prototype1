7b1538bb6254057e8020eb813ac6f436e370a70f
ghdeleo <ghdeleo@gmail.com>
2023-03-28 18:39:00 -0400

    meal request meal type
    added code for drop down menu

--- src/main/java/edu/wpi/romanticraijuu/controllers/ServiceRequestController.java
+++ src/main/java/edu/wpi/romanticraijuu/controllers/ServiceRequestController.java
@@ -5,7 +5,10 @@ import edu.wpi.romanticraijuu.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.awt.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 public class ServiceRequestController {
 
@@ -15,24 +18,35 @@ public class ServiceRequestController {
   @FXML MFXTextField nameField;
   @FXML MFXTextField locationField;
   @FXML MFXTextField notesField;
+  @FXML ChoiceBox mealTypeBox;
 
   private String name;
   private String location;
   private String notes;
+  private String mealType;
+
+  ObservableList<String> mealTypeList =
+      FXCollections.observableArrayList("Chicken", "Beef", "Fish");
 
   @FXML
   public void initialize() {
     cancelButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     clearButton.setOnMouseClicked(event -> clear());
     submitButton.setOnMouseClicked(event -> submit());
+    mealTypeBox.setValue("Select Meal");
+    mealTypeBox.setItems(mealTypeList);
   }
 
   @FXML
   public void submit() {
     name = nameField.getText();
     location = locationField.getText();
+    mealType = mealTypeBox.getValue().toString();
+    if (mealType == "Select Meal") {
+      mealType = "";
+    }
     notes = notesField.getText();
-    System.out.println(name + " " + location + " " + notes);
+    System.out.println(name + " " + location + " " + mealType + " " + notes);
     Navigation.navigate(Screen.HOME);
   }
 
