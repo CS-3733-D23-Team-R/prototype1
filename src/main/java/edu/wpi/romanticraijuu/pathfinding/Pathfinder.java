7d55df027d51b77e7eccad7b2c6ec6d3bd27851b
TurtleCat02 <bcspenguin1@gmail.com>
2023-03-29 17:14:56 -0400

    Fixed breadth first search

--- src/main/java/edu/wpi/romanticraijuu/pathfinding/Pathfinder.java
+++ src/main/java/edu/wpi/romanticraijuu/pathfinding/Pathfinder.java
@@ -16,6 +16,7 @@ public class Pathfinder {
   private ArrayList<String> findNeighboringNodes(String nodeID, String endID) throws Exception {
     // ArrayList<Node> neighbors = new ArrayList<Node>();
     ArrayList<String> neighbors = edges.getConnectedNodes(nodeID);
+    ArrayList<String> toRemove = new ArrayList<>();
     for (String neighbor : neighbors) {
       if (neighbor.equals(endID)) {
         continue;
@@ -23,36 +24,42 @@ public class Pathfinder {
       Node node = nodes.getNodeByID(neighbor);
       String type = node.getNodeType();
       if (!type.equals("HALL") && !type.equals("ELEV") && !type.equals("STAI")) {
-        neighbors.remove(neighbor);
+        toRemove.add(neighbor);
       }
     }
+    neighbors.removeAll(toRemove);
 
     return neighbors;
   }
 
-  public Path breadthFirstSearch(String startID, String endID) throws Exception {
+  public Path breadthFirstSearch(String startID, String endID) {
     Path path = new Path();
     HashMap<String, String> cameFrom = new HashMap<>();
-    LinkedList<String> queue = new LinkedList<>(findNeighboringNodes(startID, endID));
+    LinkedList<String> queue = new LinkedList<>();
+    queue.add(startID);
     String currentNode;
     while (!queue.isEmpty()) {
       currentNode = queue.remove();
       if (currentNode.equals(endID)) {
         break;
       }
-      ArrayList<String> neighbors = findNeighboringNodes(currentNode, endID);
+      ArrayList<String> neighbors = edges.getConnectedNodes(currentNode);
+      // System.out.println("Neighbors: " + neighbors);
       for (String neighbor : neighbors) {
+        // System.out.println("Neighbors: " + neighbor);
         if (!cameFrom.containsKey(neighbor)) {
           queue.addLast(neighbor);
           cameFrom.put(neighbor, currentNode);
+          // System.out.println("Put " + currentNode + " into cameFrom for " + neighbor);
         }
       }
     }
-
-    currentNode = endID;
-    while (!currentNode.equals(startID)) {
-      path.add(currentNode);
-      currentNode = cameFrom.get(currentNode);
+    // System.out.println("End id: " + endID);
+    String currentPathNode = endID;
+    while (!currentPathNode.equals(startID)) {
+      path.add(currentPathNode);
+      currentPathNode = cameFrom.get(currentPathNode);
+      // System.out.println(currentPathNode);
     }
 
     return path;
